6762b5476671a6e5a19ab33c06c9a60724958be9
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-07-08 19:42:13 +0200

    added footer

---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import React from "react";
+
+export default function Chat() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+}
