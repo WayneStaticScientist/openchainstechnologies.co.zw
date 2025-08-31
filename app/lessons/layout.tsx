4245dba9d6e29ed8c8a398c2f9bb16fe71f3ecbf
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-08-31 18:26:10 +0200

    ad

--- app/lessons/layout.tsx
+++ app/lessons/layout.tsx
@@ -11,23 +11,22 @@ export default function LayoutLessons({
 }>) {
   return (
     <>
-        <TutorialsTopBar />
-        <Header />
-        <section
-          className="singleblog-section blogpage-section gradient "
-          id="single"
+      <TutorialsTopBar />
+      <Header />
+      <section
+        className="singleblog-section blogpage-section gradient "
+        id="single"
+      >
+        <div
+          className="container wow fadeIn  tw:px-0! tw:max-w-[1200px]! tw:mx-auto! "
+          data-wow-duration="2s"
+          data-wow-delay="0.3s"
         >
-          <div
-            className="container wow fadeIn  tw:px-0! tw:max-w-[1200px]! tw:mx-auto! "
-            data-wow-duration="2s"
-            data-wow-delay="0.3s"
-          >
-            <div className="row tw:xl:w-[1200px]!">{children}</div>
-          </div>
-        </section>
+          <div className="row tw:xl:w-[1200px]!">{children}</div>
+        </div>
+      </section>
 
-        <Footer />
-      </>
+      <Footer />
     </>
   );
 }
