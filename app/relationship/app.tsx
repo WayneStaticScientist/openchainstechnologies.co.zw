d94d55b95668a45538c7f18a89fedbcf6b47295c
WayneScientist <38685831+WayneStaticScientist@users.noreply.github.com>
2025-09-07 18:15:44 +0200

    Update app.tsx

--- app/relationship/app.tsx
+++ app/relationship/app.tsx
@@ -35,8 +35,17 @@ export default function ContactPage() {
         }
       />
       <section className="float-left w-100 position-relative contact-help-con padding-top padding-bottom main-box text-center">
-          I want a relationship with you , i love you at Liliota , You mean so much to me , Hope you will be there for me
-     </section>  
+         <h2>Wayne</h2>
+           <p>I want a relationship with you , i love you at Liliota , You mean so much to me , Hope you will be there for me
+     </p>
+      <br/>
+        <hr/>
+      <h2>Liliota</h2>
+      <p>
+      I want to be in a relationship only if I am to be promised less troubles.I can't be in a relationship where the compromisations are greater than the happy days .ndichitukwa evrytime zvakaomahazvona
+      </p>
+      
+      </section>  
       <Footer />
     </>
   );
